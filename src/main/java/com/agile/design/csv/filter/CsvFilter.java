f9ee75f76c7bba074c58cbecec8e5f59fcab5814
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-22 00:17:56 +0200

    fixed test exclude when ivi is not a number

--- src/main/java/com/agile/design/csv/filter/CsvFilter.java
+++ src/main/java/com/agile/design/csv/filter/CsvFilter.java
@@ -2,6 +2,7 @@ package com.agile.design.csv.filter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class CsvFilter {
 
@@ -23,9 +24,10 @@ public class CsvFilter {
         String[] fields = invoice.split(",");
         final String iva = fields[FIELDS_POSITION.IVA.ordinal()];
         final String igic = fields[FIELDS_POSITION.IGIC.ordinal()];
+        final Pattern pattern = Pattern.compile("\\d+(\\.\\d+)?");
         if(
             (iva.isEmpty() || igic.isEmpty()) && 
-            !(iva.isEmpty() && iva.isEmpty())
+            (pattern.matcher(iva).matches() && !igic.isEmpty())
             ){
             result.add(invoice);
         }
