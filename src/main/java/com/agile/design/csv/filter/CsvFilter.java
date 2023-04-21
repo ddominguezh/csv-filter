af30e7a8d78b9b127980d7b016812c56a4abf287
Diego Domínguez Herreros <diegoalbert500@gmail.com>
2023-04-21 23:51:59 +0200

    fixed test exclude lines with both tax fields populated as they are exclusive

--- src/main/java/com/agile/design/csv/filter/CsvFilter.java
+++ src/main/java/com/agile/design/csv/filter/CsvFilter.java
@@ -1,11 +1,32 @@
 package com.agile.design.csv.filter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CsvFilter {
 
+    private enum FIELDS_POSITION {
+        NUM_FACTURA,
+        FECHA,
+        BRUTO,
+        NETO,
+        IVA,
+        IGIC,
+        CONCEPTO,
+        CIF,
+        NIF
+    }
     public List<String> filter(List<String> lines) {
-        return lines;
+        List<String> result = new ArrayList<String>();
+        result.add(lines.get(0));
+        final String invoice = lines.get(1);
+        String[] fields = invoice.split(",");
+        final String iva = fields[FIELDS_POSITION.IVA.ordinal()];
+        final String igic = fields[FIELDS_POSITION.IGIC.ordinal()];
+        if(iva.isEmpty() || igic.isEmpty()){
+            result.add(invoice);
+        }
+        return result;
     }
     
 }
